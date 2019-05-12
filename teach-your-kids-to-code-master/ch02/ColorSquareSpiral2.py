5e5a37f6bdd85637f88edcb58c75cad2f182df16
molychin <molychin@qq.com>
2019-05-13 00:06:52 +0800

    2019.05.12.23:28 molychin

--- teach-your-kids-to-code-master/ch02/ColorSquareSpiral2.py
+++ teach-your-kids-to-code-master/ch02/ColorSquareSpiral2.py
@@ -5,7 +5,7 @@ colors=['red', 'yellow', 'blue', 'green']
 for x in range(200):
     t.pencolor(colors[x%4])
     t.forward(x)
-    t.left(87)    #87/91/93/97
+    t.left(147)    #87/91/93/97   /46/61/121/147
 
 
 
