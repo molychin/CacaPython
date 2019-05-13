a1a308d0c2423236370533c6f6effce77cff45bc
molychin <molychin@qq.com>
2019-05-13 14:49:10 +0800

    2019.05.13.14:48 molychin

---
+++ Teach_Your_kids_to_Code003.md
@@ -0,0 +1,9 @@
+```
+Created on Sat May 11 23:17:01 2019  
+@author: molychin@qq.com  
+Teach Your Kids to Code  
+```  
+
+---
+## **Teach Your Kids to Code 003**
+### Numbers aNd Variables:PythoN does the math
--- teach-your-kids-to-code-master/ch02/ColorSpiral.py
+++ teach-your-kids-to-code-master/ch02/ColorSpiral.py
@@ -5,11 +5,15 @@ turtle.Turtle().screen.delay(0)   #绘画没有延迟
 turtle.bgcolor("black")
 
 # You can choose between 2 and 6 sides for some cool shapes!
-sides = 6
+sides = 6    #6/5/4
 colors = ["red", "yellow", "blue", "orange", "green", "purple"]
 for x in range(300):
     t.pencolor(colors[x%sides])
     t.forward(x * 2/sides + x)
-    t.left(360/sides + 1)
+    t.left(300/sides + 1)
+    t.left(90)
     t.width(x*sides/200)
+
+
+
 
