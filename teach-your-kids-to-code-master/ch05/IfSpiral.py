a0f3389cb6db6dcbf3dce2f31d0d125069f9465a
molychin <molychin@qq.com>
2019-05-16 22:54:40 +0800

    2019.05.16.22:43 molychin

--- teach-your-kids-to-code-master/ch05/IfSpiral.py
+++ teach-your-kids-to-code-master/ch05/IfSpiral.py
@@ -3,9 +3,9 @@
 print("Working...")
 import turtle
 t = turtle.Pen()
-turtle.Turtle().screen.delay(0)   #绘画没有延迟
-t.width(2)
-for x in range(100):
-    t.forward(x*2)
-    t.left(130)        #89/134
+#turtle.Turtle().screen.delay(0)   #绘画没有延迟
+t.width(3)
+for x in range(10):
+    t.forward(x*20)
+    t.left(89)        #89/134
 #print("Okay, we're done!")
